f443accb2a55ca82e77c0c38dcc862dca174274e
Sayan Datta <iamsayan@users.noreply.github.com>
2018-08-15 13:31:02 +0530

    Delete color.picker.js

--- admin/assets/js/color.picker.js
+++
@@ -1,8 +0,0 @@
-(function( $ ) {
-    $(function() {
-         
-        // Add Color Picker to all inputs that have 'color-field' class
-        $( '.ufc-color-picker' ).wpColorPicker();
-         
-    });
-})( jQuery );
